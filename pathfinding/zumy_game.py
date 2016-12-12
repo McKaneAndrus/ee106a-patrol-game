4b961275f6c8f0f06ec968baa4b1d15407b13747
Class Account <ee106a-abd@imail.EECS.Berkeley.EDU>
2016-12-11 19:44:36 -0800

    test case now includes a patrol with vision being blocked

--- pathfinding/zumy_game.py
+++ pathfinding/zumy_game.py
@@ -1,9 +1,15 @@
 from player import Player
 
-game_map = [[1, 1, 1, 1, 1],[1, 0, 0, 0, 1],[1, 0, 2, 1, 1],[1, 0, 0, 0, 1],[1, 1, 1, 1, 1]]
-start = (1, 2)
-goal = (3, 2)
-player = Player(game_map, start, goal, [])
+game_map = [[1, 1, 1, 1, 1, 1, 1],
+			[1, 0, 0, 0, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 1],
+			[1, 0, 1, 0, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 1],
+			[1, 1, 1, 1, 1, 1, 1]]
+start = (1, 1)
+goal = (4, 5)
+patrol = ((3, 4), (0, -1))
+player = Player(game_map, start, goal, [patrol])
 
 next_step = player.getNextStep()
 print(next_step)
